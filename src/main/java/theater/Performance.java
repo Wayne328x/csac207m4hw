75d3d3442e33a4f2ff90ae98b49bd8b8e881918f
wwwww <wayne20040328@gmail.com>
2025-11-18 19:32:37 -0500

    Task 1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -5,11 +5,29 @@ package theater;
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private final int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    /**
+     * Return the play ID for this performance.
+     *
+     * @return the ID of the play
+     */
+    public String getPlayID() {
+        return playID;
+    }
+
+    /**
+     * Return the audience size.
+     *
+     * @return number of people attending
+     */
+    public int getAudience() {
+        return audience;
     }
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,33 @@
 package theater;
 
+/**
+ * Class representing a play..
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * Return the play's name.
+     *
+     * @return the name of this play
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Return the play's type.
+     *
+     * @return the type of this play
+     */
+    public String getType() {
+        return type;
     }
 }
